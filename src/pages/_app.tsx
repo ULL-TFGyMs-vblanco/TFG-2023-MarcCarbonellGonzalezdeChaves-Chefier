8a708a6b907805719e4643824e3a3ca0fe01e1c1
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-03-03 21:27:35 +0000

    Cambio fuente de letra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Raleway } from '@next/font/google';
+
+const raleway = Raleway({ subsets: ['latin'], variable: '--font-raleway' });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${raleway.variable} ${raleway.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
