afb8e749522e7fe1dcc3f16ec5655e1cf54b1e95
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-03-03 12:06:16 +0000

    Barra de navegación - barra de búsqueda

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import styles from '../styles/Navbar.module.css';
 import Image from 'next/image';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 export const Navbar: React.FC = () => {
   const [toggle, setToggle] = useState(false);
@@ -25,7 +26,20 @@ export const Navbar: React.FC = () => {
             </Link>
           </div>
           <div className={styles.right__elements}>
-            <input type='text' placeholder='Search' className={styles.search} />
+            <div className={styles.search}>
+              <button className={styles.search__icon}>
+                <AiOutlineSearch
+                  className={styles.e}
+                  size={15}
+                  color='darkgrey'
+                />
+              </button>
+              <input
+                className={styles.search__input}
+                type='text'
+                placeholder='Search'
+              />
+            </div>
             <ul className={styles.links}>
               <li className={styles.link}>
                 <Link href='/recipes'>Recipes</Link>
@@ -52,6 +66,15 @@ export const Navbar: React.FC = () => {
             animation ? styles.in__animation : styles.out__animation
           }`}
         >
+          <Link className={styles.toggle__avatar} href='/profile'>
+            <Image
+              src='/images/avatar_default.jpg'
+              alt='Default avatar'
+              width={40}
+              height={40}
+              style={{ borderRadius: '50%', display: 'block' }}
+            />
+          </Link>
           <ul className={styles.toggle__links}>
             <li className={styles.toggle__link}>
               <Link href='/recipes'>Recipes</Link>
