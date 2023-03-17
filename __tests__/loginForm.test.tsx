c824e70a73aa10785ede1994bc87a4d12641251a
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-03-17 08:36:19 +0000

    Actualización de tests

--- __tests__/loginForm.test.tsx
+++ __tests__/loginForm.test.tsx
@@ -17,25 +17,25 @@ describe('Login form', (): void => {
   afterEach(cleanup);
 
   it('should render', (): void => {
-    render(<LoginForm submitHandler={mockLogin} />);
+    render(<LoginForm onSubmit={mockLogin} />);
   });
   it('should render form card', (): void => {
-    render(<LoginForm submitHandler={mockLogin} />);
+    render(<LoginForm onSubmit={mockLogin} />);
 
     screen.getByTestId('form-card');
   });
   it('should render title', (): void => {
-    render(<LoginForm submitHandler={mockLogin} />);
+    render(<LoginForm onSubmit={mockLogin} />);
 
     screen.getByText('Log In');
   });
   it('should render form', (): void => {
-    render(<LoginForm submitHandler={mockLogin} />);
+    render(<LoginForm onSubmit={mockLogin} />);
 
     screen.getByTestId('login-form');
   });
   it('should render form fields', (): void => {
-    render(<LoginForm submitHandler={mockLogin} />);
+    render(<LoginForm onSubmit={mockLogin} />);
 
     const fields = screen.getAllByTestId('form-field');
     expect(fields.length).toBe(2);
@@ -43,13 +43,13 @@ describe('Login form', (): void => {
     screen.getByText('Password');
   });
   it('should render form checkbox', (): void => {
-    render(<LoginForm submitHandler={mockLogin} />);
+    render(<LoginForm onSubmit={mockLogin} />);
 
     screen.getByTestId('form-checkbox');
     screen.getByText('show password');
   });
   it('should show password when clicking checkbox', (): void => {
-    render(<LoginForm submitHandler={mockLogin} />);
+    render(<LoginForm onSubmit={mockLogin} />);
 
     const checkbox = screen.getByTestId('checkbox');
     expect(screen.getByTestId('password-input')).toHaveProperty(
@@ -60,23 +60,23 @@ describe('Login form', (): void => {
     expect(screen.getByTestId('password-input')).toHaveProperty('type', 'text');
   });
   it('should render submit button', (): void => {
-    render(<LoginForm submitHandler={mockLogin} />);
+    render(<LoginForm onSubmit={mockLogin} />);
 
     screen.getByTestId('submit-button');
     screen.getByText('Log in');
   });
-  it('should render submit button', async () => {
-    render(<LoginForm submitHandler={mockLogin} />);
+  it('should call submit handler with field values when clicking submit button', async () => {
+    render(<LoginForm onSubmit={mockLogin} />);
 
     const email = screen.getByTestId('email-input');
-    fireEvent.input(email, { target: { value: 'marc@gmail.com' } });
+    fireEvent.input(email, { target: { value: 'user@gmail.com' } });
     const password = screen.getByTestId('password-input');
     fireEvent.input(password, { target: { value: 'Password1' } });
     const submit = screen.getByTestId('submit-button');
     fireEvent.submit(submit);
     await waitFor(() => expect(screen.queryAllByRole('alert')).toHaveLength(0));
     expect(mockLogin).toBeCalledWith({
-      email: 'marc@gmail.com',
+      email: 'user@gmail.com',
       password: 'Password1',
       showPassword: false,
     });
