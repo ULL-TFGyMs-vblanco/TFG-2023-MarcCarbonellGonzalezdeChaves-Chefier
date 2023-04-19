317887b16ea7c1241dc1d7a243616165b9f9aa2b
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-04-19 20:45:26 +0100

    CORS Configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,14 +3,11 @@ import bodyParser from 'koa-bodyparser';
 import './db/mongoose';
 import { defaultRouter } from './routers/default.routes';
 import { userRouter } from './routers/user.routes';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const cors = require('cors');
 
 export const app = new Koa();
 
 app
   .use(bodyParser())
-  .use(cors({ origin: '*' }))
   .use(userRouter.routes())
   .use(userRouter.allowedMethods())
   .use(defaultRouter.routes());
