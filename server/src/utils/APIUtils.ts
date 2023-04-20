126738134ece710d8d1126850a3014f474733df1
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-04-20 10:58:49 +0100

    Refactoring

--- server/src/utils/APIUtils.ts
+++ server/src/utils/APIUtils.ts
@@ -1,4 +1,4 @@
-import { User } from 'src/models/user';
+import { User } from '../models/user';
 import bcrypt from 'bcrypt';
 
 export default class APIUtils {
@@ -15,7 +15,6 @@ export default class APIUtils {
       username: username,
     });
     let suffix = 1;
-
     while (existingUser) {
       existingUser = await User.findOne({
         username: username + suffix,
@@ -23,12 +22,28 @@ export default class APIUtils {
       suffix++;
       if (!existingUser) username += suffix - 1;
     }
-
     let user = new User({ username, email });
+
+    // Google and Github users have image but don't have passwords
     if (request.body.image) {
+      let username = request.body.username.toLowerCase().replace(/ /g, '_');
+      let existingUser = await User.findOne({
+        username: username,
+      });
+      let suffix = 1;
+      while (existingUser) {
+        existingUser = await User.findOne({
+          username: username + suffix,
+        });
+        suffix++;
+        if (!existingUser) username += suffix - 1;
+      }
       const image = request.body.image;
       user = new User({ username, email, image });
+
+      // Credential users have password but don't have image
     } else if (request.body.password) {
+      const username = request.body.username.toLowerCase().replace(/ /g, '_');
       const password = await bcrypt.hash(request.body.password, 10);
       user = new User({ username, email, password });
     }
