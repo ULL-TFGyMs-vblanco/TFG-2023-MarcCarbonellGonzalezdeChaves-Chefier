f35a65e226b4bff17e5c52f580c165ec81641607
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-04-20 12:31:08 +0100

    Refactoring

--- server/src/utils/APIUtils.ts
+++ server/src/utils/APIUtils.ts
@@ -10,25 +10,11 @@ export default class APIUtils {
   public static buildUserDocument = async (request: any) => {
     const email = request.body.email;
 
-    let username = request.body.username.toLowerCase().replace(/ /g, '_');
-    if (request.body.username.length > 20) {
-      username = username.substring(0, 20);
-    }
-    let existingUser = await User.findOne({
-      username: username,
-    });
-    let suffix = 1;
-    while (existingUser) {
-      existingUser = await User.findOne({
-        username: username + suffix,
-      });
-      suffix++;
-      if (!existingUser) username += suffix - 1;
-    }
-    let user = new User({ username, email });
-
     // Google and Github users have image but don't have passwords
     if (request.body.image) {
+      if (request.body.username.length > 20) {
+        request.body.username = request.body.username.substring(0, 20).trim();
+      }
       let username = request.body.username.toLowerCase().replace(/ /g, '_');
       let existingUser = await User.findOne({
         username: username,
@@ -42,15 +28,15 @@ export default class APIUtils {
         if (!existingUser) username += suffix - 1;
       }
       const image = request.body.image;
-      user = new User({ username, email, image });
+      const user = new User({ username, email, image });
+      return user;
 
       // Credential users have password but don't have image
-    } else if (request.body.password) {
+    } else {
       const username = request.body.username.toLowerCase().replace(/ /g, '_');
       const password = await bcrypt.hash(request.body.password, 10);
-      user = new User({ username, email, password });
+      const user = new User({ username, email, password });
+      return user;
     }
-
-    return user;
   };
 }
