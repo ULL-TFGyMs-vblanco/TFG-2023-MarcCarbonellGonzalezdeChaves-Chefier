34a3ea9e56a9539aa7df0e38f25cd8ecc4617cc0
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-04-28 14:49:15 +0100

    Fixing errors

--- src/utils/ImagekitUtils.ts
+++ src/utils/ImagekitUtils.ts
@@ -7,11 +7,12 @@ export default class ImagekitUtils {
     formData.append('upload_preset', 'posts-images');
     formData.append('cloud_name', 'duwhgqlfk');
     try {
-      const res = await axios.post(
-        'https://api.cloudinary.com/v1_1/duwhgqlfk/image/upload',
-        formData,
-        { headers: { 'Content-Type': 'multipart/form-data' } }
-      );
+      const res = await axios({
+        method: 'post',
+        url: 'https://api.cloudinary.com/v1_1/duwhgqlfk/image/upload',
+        data: formData,
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
       return res.data['secure_url'];
     } catch (err: any) {
       throw new Error(JSON.stringify(err.response.data.error));
