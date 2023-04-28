f3cab55128cd1a01e214b14cf9f70c03b08f24ab
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-04-28 22:32:04 +0100

    Refactoring

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -4,44 +4,25 @@ import { getSession } from 'next-auth/react';
 
 const RecipeService = {
   postRecipe: async (url: string, recipe: NewRecipeData) => {
+    const session = await getSession();
+    const formData = new FormData();
+    formData.append('image', recipe.image);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { image, ...recipeData } = recipe;
+    formData.append('recipe', JSON.stringify(recipeData));
+    formData.append('provider', session?.user.provider as string);
     try {
-      // const imageUrl = await ImagekitUtils.uploadImage(recipeData.image);
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      // const recipe = { ...recipeData, image: imageUrl };
-      const session = await getSession();
-      const formData = new FormData();
-      formData.append('image', recipe.image);
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { image, ...recipeData } = recipe;
-      formData.append('recipe', JSON.stringify(recipeData));
-      formData.append('provider', session?.user.provider as string);
-      try {
-        await axios({
-          method: 'post',
-          url: url,
-          data: formData,
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            Authorization: `Bearer ${session?.user.accessToken}`,
-          },
-        });
-        // await axios.post(
-        //   url,
-        //   {
-        //     provider: session?.user.provider,
-        //     recipe,
-        //   },
-        //   {
-        //     headers: {
-        //       Authorization: `Bearer ${session?.user.accessToken}`,
-        //     },
-        //   }
-        // );
-      } catch (err: any) {
-        throw new Error(err.response.data.error);
-      }
+      await axios({
+        method: 'post',
+        url: url,
+        data: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${session?.user.accessToken}`,
+        },
+      });
     } catch (err: any) {
-      throw new Error(err);
+      throw new Error(err.response.data.error.message);
     }
   },
 };
