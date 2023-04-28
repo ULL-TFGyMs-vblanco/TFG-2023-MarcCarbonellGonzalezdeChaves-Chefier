6772408e36e7928ab9204309cd6a47fd0cbf1381
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-04-28 19:21:48 +0100

    Fixing errors

--- server/dist/utils/CtxUtils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CtxUtils {
-}
-exports.default = CtxUtils;
-CtxUtils.setResponse = (response, status, body) => {
-    response.status = status;
-    response.body = body;
-};
