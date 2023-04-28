7f1fe4376c8fd6fdd0e94a057c84481ff9ba7503
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-04-28 15:35:37 +0100

    Fixing errors

--- server/src/services/recipe.api.ts
+++ server/src/services/recipe.api.ts
@@ -21,27 +21,42 @@ export const getRecipes = async (
 
 // Post a recipe
 export const postRecipe = async ({ response, request }: Context) => {
-  const recipe = new Recipe(request.body.recipe);
-  await Recipe.create(recipe)
-    .then((recipe) => {
-      utils.setResponse(response, 200, { recipe });
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        const errors = Object.keys(err.errors).map((key) => {
-          return { message: err.errors[key].message, field: key };
-        });
-        utils.setResponse(response, 400, {
-          error: { message: err._message, errors: errors },
-          request: request.body,
-        });
-      } else {
-        utils.setResponse(response, 500, {
-          error: { message: err },
-          request: request.body,
-        });
-      }
+  try {
+    const res = await utils.uploadImage(
+      request.files.image,
+      request.body.recipe.name,
+      `/images/posts/${request.body.recipe.username}}`
+    );
+    request.body.recipe.image = res.url;
+    const fileID = res.fileId;
+    const recipe = new Recipe(request.body.recipe);
+    await Recipe.create(recipe)
+      .then((recipe) => {
+        utils.setResponse(response, 200, { recipe });
+      })
+      .catch((err) => {
+        if (err.name === 'ValidationError') {
+          const errors = Object.keys(err.errors).map((key) => {
+            return { message: err.errors[key].message, field: key };
+          });
+          utils.setResponse(response, 400, {
+            error: { message: err._message, errors: errors },
+            request: request.body,
+          });
+        } else {
+          utils.setResponse(response, 500, {
+            error: { message: err },
+            request: request.body,
+          });
+        }
+        utils.deleteImage(fileID);
+      });
+  } catch (err) {
+    utils.setResponse(response, 500, {
+      error: { message: err },
+      request: request.body,
     });
+  }
 };
 
 module.exports = {
