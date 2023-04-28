541c0f973919b8ce28e03af306749b6eaeb48292
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-04-28 14:05:18 +0100

    Fixing errors

--- src/utils/ImagekitUtils.ts
+++ src/utils/ImagekitUtils.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export default class ImagekitUtils {
-  public static uploadImage = async (image: string) => {
+  public static uploadImage = async (image: File) => {
     const formData = new FormData();
     formData.append('file', image);
     formData.append('upload_preset', 'posts-images');
     try {
       const res = await axios.post(
-        'https://api.cloudinary.com/v1_1/duwhgqlfk/upload',
+        'https://api.cloudinary.com/v1_1/duwhgqlfk/image/upload',
         formData
       );
       return res.data['secure_url'];
