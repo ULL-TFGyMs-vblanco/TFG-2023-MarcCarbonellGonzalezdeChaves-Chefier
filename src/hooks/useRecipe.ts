a86ee1492f19dd9113e437ac6c8ba65488cfb4a7
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-05-07 12:29:05 +0100

    Refactoring

--- src/hooks/useRecipe.ts
+++ src/hooks/useRecipe.ts
@@ -1,10 +1,12 @@
 import useSWR from 'swr';
-import axios from '../../axios_config';
 
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 export function useRecipe(id: string) {
-  const { data, error, isLoading } = useSWR(`/recipe/${id}`, fetcher);
+  const { data, error, isLoading } = useSWR(
+    `https://chefier-backend-git-develop-tfg-marccarbonell.vercel.app/api/recipe/${id}`,
+    fetcher
+  );
 
   return {
     recipe: data,
