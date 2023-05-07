9d95e7552fb623bfa49b4e7b11ef0829b56ab8f5
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-05-07 12:13:56 +0100

    Refactoring

--- src/hooks/useRecipe.ts
+++ src/hooks/useRecipe.ts
@@ -1,7 +1,13 @@
 import useSWR from 'swr';
 import axios from '../../axios_config';
 
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+const fetcher = (url: string) =>
+  axios
+    .get(url)
+    .then((res) => res.data)
+    .catch(() => {
+      throw new Error('An error occurred while fetching the data.');
+    });
 
 export function useRecipe(id: string) {
   const key = `/recipe/${id}`;
