e23d2e48ba31672db0b86305b0013eeaba0d67d1
MarcCarbonellG <marc.carbonell.gchaves@gmail.com>
2023-05-11 15:24:04 +0100

    Refactoring

--- src/hooks/useRecipes.ts
+++ src/hooks/useRecipes.ts
@@ -3,9 +3,11 @@ import axios from '../../axios_config';
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
-export function useRecipes(pageIndex: number, filters: string) {
+export function useRecipes(pageIndex: number, filters?: string) {
   const { data, error, isLoading } = useSWR(
-    `/recipes?page=${pageIndex}${filters}`,
+    filters
+      ? `/recipes?page=${pageIndex}&${filters}`
+      : `/recipes?page=${pageIndex}`,
     fetcher,
     {
       revalidateOnMount: true,
